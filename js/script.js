2f03b2d8fae8b3e2fa2bc74e1bc5796f7cf82dd0
MFDR-2402 <mfdr2402@gmail.com>
2025-04-23 20:06:40 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+let myObj = {
+    // key = value  
+    Name: "BMW",
+    model: "X99",
+    color: "MateBlack",
+    CC: "4000",
+    price: 5000000,
+    FullName: function () {
+        console.log("Hi im BMW X99 MateBLack edition");    
+    }
+}
+
+myObj.FullName()
+
+
+// for (initialization/declaration; condition ; increment/decrement) {
+//     code to be executed
+// }
+
+
+
+for ( let i = 1; i <= 50; i++ ) {
+    for(let j = 1; j <= 10 ; j++){
+        console.log(i + " X " + j + " = " + i*j)
+    }
+}
+
+
+
+
+
+
